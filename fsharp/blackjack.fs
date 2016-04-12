55fee7d953de551fb20538bfbbc9761ebdd226d1
Ben C Robinson <bcrob89@gmail.com>
2016-04-12 20:04:13 +0100

    implemented initial functions

--- fsharp/blackjack.fs
+++ fsharp/blackjack.fs
@@ -1,73 +1,142 @@
-module Blackjack
+module Helpers =
+    let rand = new System.Random()
 
-type Picture =
-    | Ace
-    | Jack
-    | Queen
-    | King
+    let swap (a: _[]) x y =
+        let tmp = a.[x]
+        a.[x] <- a.[y]
+        a.[y] <- tmp
 
-type Face =
-    | Number of int
-    | Face of Picture
+    let shuffle a =
+        let arr = a |> List.toArray
+        Array.iteri (fun i _ -> swap arr i (rand.Next(i, Array.length arr))) arr
+        arr |> Array.toList
 
-type Suit =
-    | Spade
-    | Club
-    | Heart
-    | Diamond
+module Blackjack =
+    type Picture =
+        | Jack
+        | Queen
+        | King
 
-type Card = | Card of Suit * Face
+    type Face =
+        | Ace
+        | Number of int
+        | Face of Picture
 
-type Deck = Deck of Card list
+    type Suit =
+        | Spade
+        | Club
+        | Heart
+        | Diamond
 
-type Shoe = Shoe of Card list
+    type Card = | Card of Suit * Face
 
-type Hand = Hand of Card list
+    type Deck = Deck of Card list
 
-type HandValue =
-    | Bust of int
-    | BlackJack
-    | Soft of int
-    | Hard of int
+    type Shoe = Shoe of Card list
 
-type Game = {
-    Deck : Deck
-    Dealer : Hand
-    Players : Hand list
-}
+    type Hand = Hand of Card list
 
-let newDeck : Deck =
-    let cards (suit : Suit) =
-        seq {
-            yield Card(suit, Face(Ace))
-            for i in 2..10 do
-                yield Card(suit, Number(i))
-            yield Card(suit, Face(Jack))
-            yield Card(suit, Face(Queen))
-            yield Card(suit, Face(King))
-        }
+    type HandValue =
+        | Bust of int
+        | BlackJack
+        | Value of soft : int * hard : int
 
-    let allCards =
-        seq {
-            for s in [Club; Heart; Diamond; Spade] do
-                yield! (cards s)
-        }
-        |> Seq.toList
-    Deck(allCards)
+    type Game = {
+        Deck : Deck
+        Dealer : Hand
+        Players : Hand list
+    }
 
-let shuffle (deck : Deck) : Deck =
-    deck
+    let newDeck : Deck =
+        let cards (suit : Suit) =
+            seq {
+                yield Card(suit, Ace)
+                for i in 2..10 do
+                    yield Card(suit, Number(i))
+                yield Card(suit, Face(Jack))
+                yield Card(suit, Face(Queen))
+                yield Card(suit, Face(King))
+            }
 
-let take (deck : Deck) : (Card * Deck) =
-    failwith "NI"
+        let allCards =
+            seq {
+                for s in [Club; Heart; Diamond; Spade] do
+                    yield! (cards s)
+            }
+            |> Seq.toList
+        Deck(allCards)
 
-let deal handCount (deck : Deck) : (Hand list * Deck) =
-    failwith "NI"
+    let shuffle (Deck(cards)) : Deck =
+        let cards = Helpers.shuffle cards
+        Deck(cards)
 
-let getHandValue (Hand(hand)) : HandValue =
-    failwith "NI"
+    let takeTop (Deck(cards)) : (Card * Deck) =
+        match cards with
+        | [] ->
+            failwith "No cards!"
+        | top :: deck ->
+            (top, Deck(deck))
 
-[<EntryPoint>]
-let main argv =
-    printfn "%A" argv
-    0 // return an integer exit code
+    let dealInitialHands numberOfHands (deck : Deck) : (Hand list * Deck) =
+        let (Deck(cards)) = deck
+        if (numberOfHands <= 1) then
+            failwith "Too few players!"
+        else if (numberOfHands * 2) > cards.Length then
+            failwith "Too many players!"
+        else
+            let getTopN n d =
+                let rec getTopNRec i d acc =
+                    if i = 0 then
+                        (acc, d)
+                    else
+                        let (c, d') = takeTop d
+                        getTopNRec (i - 1) d' (c :: acc)
+                getTopNRec n d []
+
+            let (firstCards, d') = getTopN numberOfHands deck
+            let (secondCards, d') = getTopN numberOfHands d'
+            let hands =
+                firstCards
+                |> List.zip secondCards
+                |> List.map (fun (firstCard, secondCard) -> [firstCard; secondCard] |> Hand )
+            (hands, d')
+
+    let getHandValue (Hand(cards)) : HandValue =
+        let getHardValue (face : Face) : int =
+            match face with
+            | Ace -> 11
+            | Number(i) -> i
+            | Face (_) -> 10
+
+        let getSoftValue (face : Face) : int =
+            match face with
+            | Ace -> 1
+            | Number(i) -> i
+            | Face (_) -> 10
+
+        let hardValue =
+            cards
+            |> List.fold (fun i (Card(_, face)) -> i + (getHardValue face)) 0
+
+        let softValue =
+            cards
+            |> List.fold (fun i (Card(_, face)) -> i + (getSoftValue face)) 0
+
+        let isBlackJack =
+            let faces = cards |> List.map (fun (Card(_, f)) -> f)
+            match faces with
+            | [Ace; f] when (getHardValue f) = 10 -> true
+            | [f; Ace] when (getHardValue f) = 10 -> true
+            | _ -> false
+
+        if isBlackJack then
+            BlackJack
+        else if softValue > 21 then
+            Bust(softValue)
+        else
+            Value(softValue, hardValue)
+
+    [<EntryPoint>]
+    let main argv =
+        printfn "%A" argv
+        0 // return an integer exit code
